ba6c2505643b8674a93196601145d8884314a534
Camilo Álvarez <camiloalvarez0521@gmail.com>
2024-04-25 23:47:15 -0500

    Add logic to change icons oin header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -3,6 +3,8 @@ import { Router } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
+import { faGauge } from '@fortawesome/free-solid-svg-icons';
+import { faStore } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
@@ -14,11 +16,28 @@ import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 export class HeaderComponent {
   public faCartShopping = faCartShopping;
   public faRightFromBracket = faRightFromBracket;
+  public faGauge = faGauge;
+  public faStore = faStore;
+  public userRole!: string;
+  public showCart = false;
+  public showDashboard = false;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) {
+    this.userRole = localStorage.getItem('role') || '';
+    this.showCart = this.router.url.includes('home');
+    this.showDashboard = this.router.url.includes('dashboard');
+  }
 
   signOut() {
     localStorage.clear();
     this.router.navigateByUrl('/auth');
+  }
+
+  goToDashboard() {
+    this.router.navigateByUrl('/admin/dashboard');
+  }
+
+  goToStore() {
+    this.router.navigateByUrl('/client/home');
   }
 }
