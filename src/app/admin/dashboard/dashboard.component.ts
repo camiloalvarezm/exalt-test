7dd1590fee9444e1c627636d8e997d62b9e79867
Camilo Álvarez <calvarez@consultec-ti.com>
2024-04-26 16:05:22 -0500

    Add modal to create products

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -7,11 +7,19 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
 import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { ModalComponent } from '../../shared/components/modal/modal.component';
+import { LoaderComponent } from '../../shared/components/loader/loader.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [HeaderComponent, CommonModule, FontAwesomeModule],
+  imports: [
+    HeaderComponent,
+    CommonModule,
+    FontAwesomeModule,
+    ModalComponent,
+    LoaderComponent,
+  ],
   providers: [CurrencyPipe],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
@@ -21,25 +29,59 @@ export class DashboardComponent implements OnInit {
   public faPenToSquare = faPenToSquare;
   public faTrashCan = faTrashCan;
   public faPlus = faPlus;
-
+  public action: string = '';
+  public showModal: boolean = false;
+  public editProductData: Product = {
+    id: '',
+    name: '',
+    description: '',
+    price: 0,
+    stock: 0,
+    quantity: 0,
+  };
   constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
-    this.productService.getProducts().subscribe((res) => {
-      this.products = res;
-      localStorage.setItem('products', JSON.stringify(this.products))
-    });
+    if (!localStorage.getItem('products')) {
+      this.productService.getProducts().subscribe((res) => {
+        this.products = res;
+        localStorage.setItem('products', JSON.stringify(this.products));
+      });
+      return;
+    }
+    this.products = JSON.parse(localStorage.getItem('products') || '[]');
   }
 
   addNewProduct() {
-    
+    this.action = 'add';
+    this.showModal = true;
   }
 
   editProduct(product: Product) {
-    console.log('editar', product);
+    this.action = 'edit';
+    this.editProductData = product;
+    this.showModal = true;
   }
 
   deleteProduct(id: string) {
     console.log('eliminar', id);
+  }
+
+  closeModal(event: boolean) {
+    this.showModal = event;
+  }
+
+  storeProduct(event: Product) {
+    console.log('producto a guardar', event);
+    const products: Product[] = JSON.parse(
+      localStorage.getItem('products') || '[]'
+    );
+    event.id = (products.length + 1).toString();
+    if (this.action == 'add') {
+      products.unshift(event);
+      localStorage.setItem('products', JSON.stringify(products));
+      this.products = products;
+    } else if (this.action == 'edit') {
+    }
   }
 }
---
+++ src/app/shared/components/modal/modal.component.ts
@@ -0,0 +1,47 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
+import { Product } from '../../../core/models/product.model';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-modal',
+  standalone: true,
+  imports: [FontAwesomeModule, CommonModule, FormsModule],
+  templateUrl: './modal.component.html',
+  styleUrl: './modal.component.scss',
+})
+export class ModalComponent {
+  @Input() show: boolean = true;
+  @Input() action: string = 'add';
+  @Input() formData: Product = {
+    id: '',
+    name: '',
+    description: '',
+    price: 0,
+    stock: 0,
+    quantity: 0,
+  };
+  @Output() closeModal = new EventEmitter<boolean>();
+  @Output() productData = new EventEmitter<any>();
+  public faCircleXmark = faCircleXmark;
+
+  onClose() {
+    this.show = false;
+    this.closeModal.emit(false);
+    this.formData = {
+      id: '',
+      name: '',
+      description: '',
+      price: 0,
+      stock: 0,
+      quantity: 0,
+    };
+  }
+
+  onSubmit() {
+    this.productData.emit(this.formData);
+    this.onClose();
+  }
+}
