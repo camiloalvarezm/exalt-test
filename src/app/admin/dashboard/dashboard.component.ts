0fad3c1e29454c94a1feee008ba397208eb2c0e3
Camilo Álvarez <calvarez@consultec-ti.com>
2024-04-26 13:25:29 -0500

    Add table to dashboard

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { CommonModule, CurrencyPipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../../shared/components/header/header.component';
+import { ProductService } from '../../api/product.service';
+import { Product } from '../../core/models/product.model';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, CommonModule, FontAwesomeModule],
+  providers: [CurrencyPipe],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
-export class DashboardComponent {}
+export class DashboardComponent implements OnInit {
+  public products: Product[] = [];
+  public faPenToSquare = faPenToSquare;
+  public faTrashCan = faTrashCan;
+  public faPlus = faPlus;
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe((res) => {
+      this.products = res;
+      localStorage.setItem('products', JSON.stringify(this.products))
+    });
+  }
+
+  addNewProduct() {
+    
+  }
+
+  editProduct(product: Product) {
+    console.log('editar', product);
+  }
+
+  deleteProduct(id: string) {
+    console.log('eliminar', id);
+  }
+}
